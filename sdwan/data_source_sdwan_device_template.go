217da04cab145ca48b02bc139fc3da19536bd25d
harshthakkar_crest <thakkar.harsh@crestdatasys.com>
2021-05-03 15:02:59 +0530

    device_template resource

--- sdwan/data_source_sdwan_device_template.go
+++ sdwan/data_source_sdwan_device_template.go
@@ -41,14 +41,14 @@ func datasourceSDWANDeviceTemplate() *schema.Resource {
 				Description: "Configuration type for  Device Template",
 			},
 			"factory_default": {
-				Type:        schema.TypeString,
+				Type:        schema.TypeBool,
 				Computed:    true,
 				Description: "Boolean flag indicating whether Device Template is factory default or not",
 			},
 			"policy_id": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "policy_id for  Device Template",
+				Description: "policyId for  Device Template",
 			},
 			"feature_template_uid_range": {
 				Type:        schema.TypeList,
@@ -93,11 +93,6 @@ func datasourceSDWANDeviceTemplate() *schema.Resource {
 				Computed:    true,
 				Description: "Time when  Device Template is created",
 			},
-			"template_configuration_edited": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Previous Template Configuration for  Device Template",
-			},
 			"rid": {
 				Type:        schema.TypeInt,
 				Computed:    true,
@@ -109,7 +104,7 @@ func datasourceSDWANDeviceTemplate() *schema.Resource {
 				Description: "Feature for Device Template",
 			},
 			"template_attached": {
-				Type:        schema.TypeString,
+				Type:        schema.TypeInt,
 				Computed:    true,
 				Description: "Number of templates attached to Device Template",
 			},
@@ -151,110 +146,218 @@ func datasourceSDWANDeviceTemplate() *schema.Resource {
 					},
 				},
 			},
+			"created_by": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "User who created Device Template",
+			},
+			"last_updated_on": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Time when Device Template last updated",
+			},
 		},
 	}
 }
 
 func dataSourceSDWANDeviceTemplateRead(d *schema.ResourceData, m interface{}) error {
 	log.Println("[DEBUG] Begining Read method ")
+
 	sdwanClient := m.(*client.Client)
-	log.Println("[DEBUG] checking %v", sdwanClient)
 	dtname := d.Get("template_name").(string)
 	datas, err := sdwanClient.GetViaURL("/dataservice/template/device")
-	log.Println("[DEBUG] after GET call")
 	if err != nil {
 		return err
 	}
 
-	if datas != nil {
-		log.Println("[DEBUG] container is nil")
-	}
-	l := len(datas.Children())
-	log.Printf("[DEBUG] length of children %d", l)
-	var data *container.Container = datas.S("data")
-	// log.Println("[DEBUG] reterive data")
+	var (
+		data     *container.Container = datas.S("data")
+		selected *container.Container
+		tid      string
+	)
 
-	var selected *container.Container
 	for _, child := range data.Children() {
 		if child.S("templateName").Data().(string) == dtname {
-			selected = child
-			d.SetId(child.S("template_name").Data().(string))
+			tid = child.S("templateId").Data().(string)
+			objurl := "dataservice/template/device/object/" + tid
+			selected, err = sdwanClient.GetViaURL(objurl)
+			if err != nil {
+				return err
+			} else {
+				d.SetId(child.S("templateName").Data().(string))
+			}
 			break
 		}
 	}
+
 	if selected == nil {
 		return fmt.Errorf("There is no Device Template named %s", dtname)
 	} else {
-		for _, key := range []string{
-			"template_name",
-			"template_description",
-			"device_type",
-			"device_role",
-			"config_type",
-			"factory_default",
-			"policy_id",
-			"connection_preference_required",
-			"connection_preference",
-			"last_updated_by",
-			"template_class",
-			"template_configuration",
-			"template_id",
-			"created_on",
-			"template_configuration_edited",
-			"feature",
-			"template_attached",
-			"feature_template_uid_range",
-			"general_templates",
-		} {
-			camelCase := snakeCaseToCamelCase(key)
-			if selected.Exists(camelCase) {
-				d.Set(key, selected.S(camelCase).Data())
-			}
-			if selected.Exists("@rid") {
-				d.Set("rid", selected.S("@rid").Data())
-			}
-		}
-		// fturcc:=snakeCaseToCamelCase("feature_template_uid_range")
-		// if selected.Exists(fturcc) {
-		// 	fturs := []string{}
-		// 	for _, ftur := range selected.S(fturcc).Children() {
-		// 		fturs = append(fturs, ftur.Data().(string))
-		// 	}
-		// 	d.Set("feature_template_uid_range", fturs)
-		// }
-		// gtcc:=snakeCaseToCamelCase("general_templates")
-		// if selected.Exists(gtcc) {
-		//     gts:=[]interface{}{}
-		//     for _,gt:=range selected.S(gtcc).Children(){
-		// 		for _,key:= range[]string{
-		// 			"template_id",
-		// 			"template_type",
-		// 		}{
-		// 			camelCase:=snakeCaseToCamelCase(key)
-		// 			if gt.Exists(camelCase){
-		// 				d.Set(key,gt.S(camelCase).Data())
-		// 			}
-		// 			stcc:=snakeCaseToCamelCase("sub_templates")
-		// 			if gt.Exists(stcc){
-		// 				sts:=[]interface{}{}
-		// 				for _,st:=range gt.S(stcc).Children(){
-		// 					for _,key:=range[]string{
-		// 						"template_id",
-		// 						"template_type",
-		// 					}{
-		// 						camelCase:=snakeCaseToCamelCase(key)
-		// 						if gt.Exists(camelCase){
+		d.Set("template_name", stripQuotes(selected.S("templateName").String()))
+		d.Set("template_description", stripQuotes(selected.S("templateDescription").String()))
+		d.Set("device_type", stripQuotes(selected.S("deviceType").String()))
 
-		// 						}
-		// 					}
-		// 				}
+		if selected.Exists("deviceRole") {
+			d.Set("device_role", stripQuotes(selected.S("deviceRole").String()))
+		}
+
+		d.Set("config_type", stripQuotes(selected.S("configType").String()))
+		d.Set("factory_default", selected.S("factoryDefault").Data().(bool))
+
+		if selected.Exists("policyId") {
+			d.Set("policy_id", stripQuotes(selected.S("policyId").String()))
+		}
+
+		if selected.Exists("connectionPreferenceRequired") {
+			d.Set("connection_preference_required", selected.S("connectionPreferenceRequired").Data().(bool))
+		}
+
+		if selected.Exists("connectionPreference") {
+			d.Set("connection_preference", selected.S("connectionPreference").Data().(bool))
+		}
+
+		if selected.Exists("lastUpdatedBy") {
+			d.Set("last_updated_by", stripQuotes(selected.S("lastUpdatedBy").String()))
+		}
+
+		if selected.Exists("lastUpdatedOn") {
+			d.Set("last_updated_on", int((selected.S("lastUpdatedOn").Data()).(float64)))
+		}
+
+		if selected.Exists("templateClass") {
+			d.Set("template_class", stripQuotes(selected.S("templateClass").String()))
+		}
+
+		if selected.Exists("templateConfiguration") {
+			d.Set("template_configuration", selected.S("templateConfiguration").Data().(string))
+		}
+
+		d.Set("template_id", stripQuotes(selected.S("templateId").String()))
+
+		if selected.Exists("createdBy") {
+			d.Set("created_by", stripQuotes(selected.S("createdBy").String()))
+		}
+
+		if selected.Exists("createdOn") {
+			d.Set("created_on", int((selected.S("createdOn").Data()).(float64)))
+		}
+
+		if selected.Exists("feature") {
+			d.Set("feature", stripQuotes(selected.S("feature").String()))
+		}
+
+		if selected.Exists("@rid") {
+			d.Set("rid", int((selected.S("@rid").Data()).(float64)))
+		}
+
+		if len(selected.S("featureTemplateUidRange").Children()) > 0 {
+			fturcontList := selected.S("featureTemplateUidRange")
+			fturList := make([]string, 0)
+			for i := 0; i < len(fturcontList.Data().([]interface{})); i++ {
+				fturList = append(fturList, stripQuotes(fturcontList.Index(i).String()))
+			}
+			d.Set("feature_template_uid_range", fturList)
+		} else {
+			d.Set("feature_template_uid_range", make([]string, 0))
+		}
+
+		if len(selected.S("generalTemplates").Children()) > 0 {
+			gtcontList := selected.S("generalTemplates")
+			gtList := make([]interface{}, 0)
+			for i := 0; i < len(gtcontList.Data().([]interface{})); i++ {
+				gtMap := make(map[string]interface{})
+
+				gtMap["template_id"] = stripQuotes(gtcontList.Index(i).S("templateId").String())
+
+				gtMap["template_type"] = stripQuotes(gtcontList.Index(i).S("templateType").String())
+
+				if len(gtcontList.Index(i).S("subTemplates").Children()) > 0 {
+					stcontList := gtcontList.Index(i).S("subTemplates")
+					stList := make([]interface{}, 0)
+					for j := 0; j < len(stcontList.Data().([]interface{})); j++ {
+						stMap := make(map[string]string)
+
+						stMap["template_id"] = stripQuotes(stcontList.Index(j).S("templateId").String())
+
+						stMap["template_type"] = stripQuotes(stcontList.Index(j).S("templateType").String())
+
+						stList = append(stList, stMap)
+					}
+
+					gtMap["sub_templates"] = stList
+				} else {
+					gtMap["sub_templates"] = make([]interface{}, 0)
+				}
+				gtList = append(gtList, gtMap)
+			}
+
+			d.Set("general_templates", gtList)
+		} else {
+			d.Set("general_templates", make([]interface{}, 0))
+		}
+		// 		for _, key := range []string{
+		// 			"template_name",
+		// 			"template_description",
+		// 			"device_type",
+		// 			"device_role",
+		// 			"config_type",
+		// 			"factory_default",
+		// 			"policy_id",
+		// 			"connection_preference_required",
+		// 			"connection_preference",
+		// 			"last_updated_by",
+		// 			"last_updated_on",
+		// 			"template_class",
+		// 			"template_configuration",
+		// 			"template_id",
+		// 			"created_by",
+		// 			"created_on",
+		// 			"feature",
+		// 			"template_attached",
+		// 		} {
+		// 			camelCase := snakeCaseToCamelCase(key)
+		// 			if selected.Exists(camelCase) {
+		// 				d.Set(key, selected.S(camelCase).Data())
+		// 			}
+		// 			if selected.Exists("@rid") {
+		// 				d.Set("rid", selected.S("@rid").Data())
 		// 			}
 		// 		}
-		//     }
-		// }
-		// if selected.Exists("feature_template_uid_range"){
-		//     d.Set("feature_template_uid_range",)
-		// }
+
+		// 		fturcc := snakeCaseToCamelCase("feature_template_uid_range")
+		// 		if len(selected.S(fturcc).Children()) > 0 {
+		// 			fturs := []string{}
+		// 			for _, ftur := range selected.S(fturcc).Children() {
+		// 				switch ftur.Data().(type) {
+		// 				case int64:
+		// 					{
+		// 						val := ftur.Data().(int64)
+		// 						str_val := strconv.FormatInt(val, 10)
+		// 						fturs = append(fturs, string(str_val))
+		// 					}
+		// 				case float64:
+		// 					{
+		// 						val := ftur.Data().(float64)
+		// 						str_val := strconv.FormatFloat(val, 'E', -1, 64)
+		// 						fturs = append(fturs, string(str_val))
+		// 					}
+		// 				case string:
+		// 					{
+		// 						fturs = append(fturs, ftur.Data().(string))
+		// 					}
+
+		// 				}
+
+		// 			}
+		// 			d.Set("feature_template_uid_range", fturs)
+		// 		}
+
+		// 		gtcc := snakeCaseToCamelCase("general_templates")
+		// 		if len(selected.S(gtcc).Children()) > 0 {
+		// 			gts := containerToMapArrayDeviceTemplate(selected.S(gtcc), []string{"template_id", "template_type", "sub_templates"})
+		// 			d.Set("general_templates", gts)
+		// 		}
 	}
+	log.Println("[DEBUG] Ending Read method ")
 	return nil
 }
--- sdwan/resource_sdwan_device_template.go
+++ sdwan/resource_sdwan_device_template.go
@@ -1,21 +1,355 @@
 package sdwan
 
-// import (
-// 	"context"
+import (
+	"log"
 
-// 	"github.com/CiscoDevNet/sdwan-go-client/models"
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-// )
+	"github.com/CiscoDevNet/sdwan-go-client/client"
+	"github.com/CiscoDevNet/terraform-provider-sdwan/vendor/github.com/CiscoDevNet/sdwan-go-client/models"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
 
-// func resourceSDWANDeviceTemplate() *schema.Resource{
-// 	return &schema.Resource{
-// 		CreateContext: resourceSDWANDeviceTemplateCreate,
-// 	}
-// }
+func resourceSDWANDeviceTemplate() *schema.Resource {
+	return &schema.Resource{
+		Create:        resourceSDWANDeviceTemplateCreate,
+		Read:          resourceSDWANDeviceTemplateRead,
+		Update:        resourceSDWANDeviceTemplateUpdate,
+		Delete:        resourceSDWANDeviceTemplateDelete,
+		Description:   "Manages Cisco SD-WAN Device Template",
+		SchemaVersion: 1,
+		Schema: map[string]*schema.Schema{
+			"template_name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Unique Device Template Name",
+			},
+			"template_description": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Long Description of Device Template",
+			},
+			"device_type": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice(validDeviceTypes, false)),
+				Description:      "Type of device for which Device Template should be created",
+			},
+			"device_role": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice(validDeviceRole, false)),
+				Description:      "Device role of the device",
+			},
+			"config_type": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice(validConfigTypes, false)),
+				Description:      "Configuration type for  Device Template",
+			},
+			"factory_default": {
+				Type:        schema.TypeBool,
+				Required:    true,
+				Description: "Boolean flag indicating whether Device Template is factory default or not",
+			},
+			"policy_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: "policyId for  Device Template",
+			},
+			"feature_template_uid_range": {
+				Type:        schema.TypeList,
+				Optional:    true,
+				Computed:    true,
+				Description: "featureTemplateUidRange for  Device Template",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"connection_preference_required": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Computed:    true,
+				Description: "connectionPreferenceRequired for Device Template",
+			},
+			"connection_preference": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Computed:    true,
+				Description: "connectionPreference for Device Template",
+			},
+			"last_updated_by": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "User who updated  Device Template last",
+			},
+			"last_updated_on": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Time when Device Template last updated",
+			},
+			"template_class": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Template Class type for  Device Template",
+			},
+			"template_configuration": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				AtLeastOneOf: []string{"template_configuration", "general_templates"},
+				Description:  "Device Template configuration",
+			},
+			"template_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Device Template id for  Device Template",
+			},
+			"general_templates": {
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Computed:    true,
+				Description: "List of Feature Templates and thier Sub Templates thourgh which Device Template is created",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"template_id": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "Template Id of Feature Template",
+						},
+						"template_type": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "Template Type of Feature Template",
+						},
+						"sub_templates": {
+							Type:        schema.TypeSet,
+							Optional:    true,
+							Computed:    true,
+							Description: "List of Sub Templates associated with feature Template",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"template_id": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "Template Id of Feature Template",
+									},
+									"template_type": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "Template Type of Feature Template",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"created_on": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Time when  Device Template is created",
+			},
+			"created_by": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "User who created Device Template",
+			},
+			"rid": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Request ID for Device Template",
+			},
+			"feature": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Feature for Device Template",
+			},
+			"template_attached": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Number of templates attached to Device Template",
+			},
+		},
+	}
+}
 
-// func resourceSDWANDeviceTemplateCreate(ctx context.Context,d *schema.ResourceData,m interface{}) diag.Diagnostics{
-// 	var diags diag.Diagnostics
-// 	deviceTemplate:=&models.DeviceTemplate{}
-// 	return diags
-// }
+func resourceSDWANDeviceTemplateCreate(d *schema.ResourceData, m interface{}) error {
+	log.Println("[DEBUG] Begining Create method ")
+	sdwanClient := m.(*client.Client)
+	deviceTemplate := &models.DeviceTemplate{
+		TemplateName:        d.Get("template_name").(string),
+		TemplateDescription: d.Get("template_description").(string),
+		DeviceType:          d.Get("device_type").(string),
+		ConfigType:          d.Get("config_type").(string),
+		FactoryDefault:      d.Get("factory_default").(bool),
+	}
+
+	if deviceTemplate.ConfigType=="template"{
+		if val,ok:=d.GetOk("connection_preference").(bool)
+		deviceTemplate.ConnectionPreferenceRequired:=d.GetOk("")
+		if val, exists := d.GetOk("general_templates"); exists {
+			gts, errs := expandGeneralTemplates(val.(*schema.Set).List())
+		}
+	}else{
+
+	}
+
+	
+	log.Println("[DEBUG] Ending Create method")
+	return nil
+}
+
+func expandGeneralTemplates(generalTemplates []interface{}) ([]map[string]interface{}, error) {
+	generalTemplate := []map[string]interface{}{}
+	for _, gt := range generalTemplates {
+		t := gt.(map[string]interface{})
+		m := make(map[string]interface{})
+
+		m["template_id"] = t["template_id"]
+		m["template_type"] = t["template_type"]
+
+		sts, errs := expandSubTemplates(t["sub_templates"].(*schema.Set).List())
+
+	}
+}
+
+func expandSubTemplates(subTemplates []interface{}) ([]map[string]interface{}, error) {
+subTemplate:
+}
+
+func resourceSDWANDeviceTemplateRead(d *schema.ResourceData, m interface{}) error {
+	log.Println("[DEBUG] Begining Read method ")
+	sdwanClient := m.(*client.Client)
+	dtid := d.Get("template_id").(string)
+	objurl := "/dataservice/template/device/object/" + dtid
+	data, err := sdwanClient.GetViaURL(objurl)
+	if err != nil {
+		return err
+	}
+
+	d.Set("template_name", stripQuotes(data.S("templateName").String()))
+	d.Set("template_description", stripQuotes(data.S("templateDescription").String()))
+	d.Set("device_type", stripQuotes(data.S("deviceType").String()))
+
+	if data.Exists("deviceRole") {
+		d.Set("device_role", stripQuotes(data.S("deviceRole").String()))
+	}
+
+	d.Set("config_type", stripQuotes(data.S("configType").String()))
+	d.Set("factory_default", data.S("factoryDefault").Data().(bool))
+
+	if data.Exists("policyId") {
+		d.Set("policy_id", stripQuotes(data.S("policyId").String()))
+	}
+
+	if data.Exists("connectionPreferenceRequired") {
+		d.Set("connection_preference_required", data.S("connectionPreferenceRequired").Data().(bool))
+	}
+
+	if data.Exists("connectionPreference") {
+		d.Set("connection_preference", data.S("connectionPreference").Data().(bool))
+	}
+
+	if data.Exists("lastUpdatedBy") {
+		d.Set("last_updated_by", stripQuotes(data.S("lastUpdatedBy").String()))
+	}
+
+	if data.Exists("lastUpdatedOn") {
+		d.Set("last_updated_on", int((data.S("lastUpdatedOn").Data()).(float64)))
+	}
+
+	if data.Exists("templateClass") {
+		d.Set("template_class", stripQuotes(data.S("templateClass").String()))
+	}
+
+	if data.Exists("templateConfiguration") {
+		d.Set("template_configuration", data.S("templateConfiguration").Data().(string))
+	}
+
+	d.Set("template_id", stripQuotes(data.S("templateId").String()))
+
+	if data.Exists("createdBy") {
+		d.Set("created_by", stripQuotes(data.S("createdBy").String()))
+	}
+
+	if data.Exists("createdOn") {
+		d.Set("created_on", int((data.S("createdOn").Data()).(float64)))
+	}
+
+	if data.Exists("feature") {
+		d.Set("feature", stripQuotes(data.S("feature").String()))
+	}
+
+	if data.Exists("@rid") {
+		d.Set("rid", int((data.S("@rid").Data()).(float64)))
+	}
+
+	if len(data.S("featureTemplateUidRange").Children()) > 0 {
+		fturcontList := data.S("featureTemplateUidRange")
+		fturList := make([]string, 0)
+		for i := 0; i < len(fturcontList.Data().([]interface{})); i++ {
+			fturList = append(fturList, stripQuotes(fturcontList.Index(i).String()))
+		}
+		d.Set("feature_template_uid_range", fturList)
+	} else {
+		d.Set("feature_template_uid_range", make([]string, 0))
+	}
+
+	if len(data.S("generalTemplates").Children()) > 0 {
+		gtcontList := data.S("generalTemplates")
+		gtList := make([]interface{}, 0)
+		for i := 0; i < len(gtcontList.Data().([]interface{})); i++ {
+			gtMap := make(map[string]interface{})
+
+			gtMap["template_id"] = stripQuotes(gtcontList.Index(i).S("templateId").String())
+
+			gtMap["template_type"] = stripQuotes(gtcontList.Index(i).S("templateType").String())
+
+			if len(gtcontList.Index(i).S("subTemplates").Children()) > 0 {
+				stcontList := gtcontList.Index(i).S("subTemplates")
+				stList := make([]interface{}, 0)
+				for j := 0; j < len(stcontList.Data().([]interface{})); j++ {
+					stMap := make(map[string]string)
+
+					stMap["template_id"] = stripQuotes(stcontList.Index(j).S("templateId").String())
+
+					stMap["template_type"] = stripQuotes(stcontList.Index(j).S("templateType").String())
+
+					stList = append(stList, stMap)
+				}
+
+				gtMap["sub_templates"] = stList
+			} else {
+				gtMap["sub_templates"] = make([]interface{}, 0)
+			}
+			gtList = append(gtList, gtMap)
+		}
+
+		d.Set("general_templates", gtList)
+	} else {
+		d.Set("general_templates", make([]interface{}, 0))
+	}
+	log.Println("[DEBUG] Ending Create method")
+	return nil
+}
+
+func resourceSDWANDeviceTemplateUpdate(d *schema.ResourceData, m interface{}) error {
+	log.Println("[DEBUG] Begining Update method ")
+	log.Println("[DEBUG] Ending Create method")
+	return nil
+}
+
+func resourceSDWANDeviceTemplateDelete(d *schema.ResourceData, m interface{}) error {
+	log.Println("[DEBUG] Begining Delete method ")
+	sdwanClient := m.(*client.Client)
+	dtid := d.Get("template_id").(string)
+	objurl := "/dataservice/template/device" + dtid
+	_, err := sdwanClient.GetViaURL(objurl)
+	if err != nil {
+		return err
+	}
+	d.SetId("")
+	log.Println("[DEBUG] Ending Create method")
+	return nil
+}
