8288ed962e4ba62317defd90bbb2cc38d3ce5a7c
Christopher Renus (crenus) <crenus@cisco.com>
2023-10-02 10:05:28 -0400

    Updating naming conventions, adding example, resolving comments

---
+++ examples/data-sources/sdwan_device/data-source.tf
@@ -0,0 +1,3 @@
+data "sdwan_device" "example" {
+  id = "f6b2c44c-693c-4763-b010-895aa3d236bd"
+}
---
+++ examples/resources/sdwan_device/resource.tf
@@ -0,0 +1,10 @@
+resource "sdwan_device" "example" {
+  device_id     = "2081c2f4-3f9f-4fee-8078-dcc8904e368d"
+  uuid          = "2081c2f4-3f9f-4fee-8078-dcc8904e368d"
+  site_id       = "400"
+  serial_number = "2AJC9DJ"
+  hostname      = "vEdge-123"
+  reachability  = "reachable"
+  status        = "normal"
+  state         = "green"
+}
