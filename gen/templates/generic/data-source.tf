a2b38c6c27d892f4b0c72ffc99a08b3f86505ba9
Chris Renus <crenus@cisco.com>
2023-10-18 12:38:31 -0400

    Simplifying data source template

--- gen/templates/generic/data-source.tf
+++ gen/templates/generic/data-source.tf
@@ -1,46 +1,10 @@
 data "sdwan_{{snakeCase .Name}}" "example" {
 {{- range  .Attributes}}
-{{- if and (not .ExcludeTest) (not .ExcludeExample) (not .TfOnly) (not .Value) (or .QueryParam)}}
-  {{- if or (eq .Type "List") (eq .Type "Set")}}
-    {{.TfName}} = [
-    {
-    {{- range  .Attributes}}
-      {{- if and (not .ExcludeTest) (not .ExcludeExample) (not .TfOnly) (not .Value)}}
-        {{- if or (eq .Type "List") (eq .Type "Set")}}
-          {{.TfName}} = [
-          {
-            {{- range  .Attributes}}
-              {{- if and (not .ExcludeTest) (not .ExcludeExample) (not .TfOnly) (not .Value)}}
-                {{- if or (eq .Type "List") (eq .Type "Set")}}
-                {{.TfName}} = [
-                {
-                {{- range  .Attributes}}
-                  {{- if and (not .ExcludeTest) (not .ExcludeExample) (not .TfOnly) (not .Value)}}
-                  {{.TfName}} = {{if eq .Type "String"}}"{{.Example}}"{{else if eq .Type "StringList"}}["{{.Example}}"]{{else}}{{.Example}}{{end}}
-                  {{- end}}
-                {{- end}}
-                }
-                ]
-        {{- else}}
-          {{.TfName}} = {{if eq .Type "String"}}"{{.Example}}"{{else if eq .Type "StringList"}}["{{.Example}}"]{{else}}{{.Example}}{{end}}
-        {{- end}}
-      {{- end}}
-    {{- end}}
-    }
-    ]
-  {{- else}}
-    {{.TfName}} = {{if eq .Type "String"}}"{{.Example}}"{{else if eq .Type "StringList"}}["{{.Example}}"]{{else}}{{.Example}}{{end}}
-  {{- end}}
-{{- end}}
-{{- end}}
-}
-]
-{{- else}}
+{{- if .QueryParam}}
 {{.TfName}} = {{if eq .Type "String"}}"{{.Example}}"{{else if eq .Type "StringList"}}["{{.Example}}"]{{else}}{{.Example}}{{end}}
 {{- end}}
 {{- end}}
-{{- end}}
 {{- if not .RemoveId}}
-  id = "f6b2c44c-693c-4763-b010-895aa3d236bd"
+id = "f6b2c44c-693c-4763-b010-895aa3d236bd"
 {{- end}}
 }
