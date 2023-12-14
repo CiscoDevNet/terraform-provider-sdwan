7620614f12787f55a83eb3d3ae125bf6c987753f
Sean Conroy <seconroy@cisco.com>
2023-12-14 11:13:03 +0000

    Fix failing test issue

--- internal/provider/resource_sdwan_traffic_data_policy_definition_test.go
+++ internal/provider/resource_sdwan_traffic_data_policy_definition_test.go
@@ -42,7 +42,6 @@ func TestAccSdwanTrafficDataPolicyDefinition(t *testing.T) {
 					resource.TestCheckResourceAttr("sdwan_traffic_data_policy_definition.test", "sequences.0.ip_type", "ipv4"),
 					resource.TestCheckResourceAttr("sdwan_traffic_data_policy_definition.test", "sequences.0.base_action", "accept"),
 					resource.TestCheckResourceAttr("sdwan_traffic_data_policy_definition.test", "sequences.0.match_entries.0.type", "appList"),
-					resource.TestCheckResourceAttr("sdwan_traffic_data_policy_definition.test", "sequences.0.match_entries.0.application_list_id", "e3aad846-abb9-425f-aaa8-9ed17b9c8d7c"),
 					resource.TestCheckResourceAttr("sdwan_traffic_data_policy_definition.test", "sequences.0.action_entries.0.type", "log"),
 					resource.TestCheckResourceAttr("sdwan_traffic_data_policy_definition.test", "sequences.0.action_entries.0.log", "true"),
 				),
@@ -52,6 +51,14 @@ func TestAccSdwanTrafficDataPolicyDefinition(t *testing.T) {
 }
 
 const testAccSdwanTrafficDataPolicyDefinitionConfig = `
+resource "sdwan_application_list_policy_object" "test" {
+  name = "TF_TEST"
+  entries = [
+    {
+      application = "netflix"
+    }
+  ]
+}
 
 
 resource "sdwan_traffic_data_policy_definition" "test" {
@@ -66,7 +73,7 @@ resource "sdwan_traffic_data_policy_definition" "test" {
 		base_action = "accept"
 		match_entries = [{
 			type = "appList"
-			application_list_id = "e3aad846-abb9-425f-aaa8-9ed17b9c8d7c"
+			application_list_id = sdwan_application_list_policy_object.test.id
 		}]
 		action_entries = [{
 			type = "log"
