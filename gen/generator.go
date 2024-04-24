f33aa6a15a671cf9ab8a5b5e9aaa4c3867fd77c9
Sean Conroy <seconroy@cisco.com>
2024-04-24 17:10:56 +0100

    Add transport routing bgp profile parcel resource and data source

--- gen/generator.go
+++ gen/generator.go
@@ -696,10 +696,15 @@ func parseProfileParcelAttribute(attr *YamlConfigAttribute, model gjson.Result) 
 		attr.TfName = SnakeCase(attr.ModelName)
 	}
 
-	if r.Get("type").String() == "object" {
+	if r.Get("type").String() == "object" || !r.Get("type").Exists() {
 		t := r.Get("oneOf.#(properties.optionType.enum.0=\"global\")")
+
+		// if value := r.Get("properties.optionType.enum.0=\"global\""); value.Exists() {
+		if value := r.Get("properties.optionType.enum.0"); value.Exists() {
+			t = r
+		}
 		if t.Exists() {
-			if t.Get("properties.value.type").String() == "string" || t.Get("properties.value.oneOf.0.type").String() == "string" {
+			if t.Get("properties.value.type").String() == "string" || t.Get("properties.value.anyOf.0.type").String() == "string" || t.Get("properties.value.oneOf.0.type").String() == "string" {
 				attr.Type = "String"
 				if value := t.Get("properties.value.minLength"); value.Exists() {
 					attr.StringMinLength = value.Int()
@@ -717,7 +722,7 @@ func parseProfileParcelAttribute(attr *YamlConfigAttribute, model gjson.Result) 
 				}
 			} else if t.Get("properties.value.type").String() == "boolean" {
 				attr.Type = "Bool"
-			} else if t.Get("properties.value.type").String() == "integer" || t.Get("properties.value.type").String() == "number" {
+			} else if t.Get("properties.value.type").String() == "integer" || t.Get("properties.value.type").String() == "number" || t.Get("properties.value.oneOf.0.type").String() == "integer" || t.Get("properties.value.oneOf.0.type").String() == "number" {
 				attr.Type = "Int64"
 				if value := t.Get("properties.value.minimum"); value.Exists() {
 					attr.MinInt = value.Int()
@@ -743,6 +748,8 @@ func parseProfileParcelAttribute(attr *YamlConfigAttribute, model gjson.Result) 
 				// if value := t.Get("properties.value.items.maximum"); value.Exists() {
 				// 	attr.MaxInt = value.Int()
 				// }
+			} else if t.Get("properties.value.const").String() == "off" || t.Get("properties.value.const").String() == "on" {
+				attr.Type = "String"
 			} else {
 				fmt.Printf("WARNING: Unsupported type: %s\n", t.Get("properties.value.type").String())
 			}
@@ -751,6 +758,9 @@ func parseProfileParcelAttribute(attr *YamlConfigAttribute, model gjson.Result) 
 			attr.Variable = true
 		}
 		d := r.Get("oneOf.#(properties.optionType.enum.0=\"default\")")
+		if value := r.Get("properties.optionType.enum.0=\"default\""); value.Exists() {
+			d = r
+		}
 		if d.Exists() && !isOneOfAttribute {
 			if value := d.Get("properties.value.enum.0"); value.Exists() {
 				attr.DefaultValue = value.String()
