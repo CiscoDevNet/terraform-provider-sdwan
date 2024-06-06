b801011b2ad1b2eaba4de500ca1257b98bad1844
Sean Conroy <seconroy@cisco.com>
2024-06-06 13:31:05 +0100

    Add policy object data prefix profile parcel resource and data source

---
+++ internal/provider/data_source_sdwan_policy_object_data_prefix_profile_parcel_test.go
@@ -0,0 +1,76 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://mozilla.org/MPL/2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+// Section below is generated&owned by "gen/generator.go". //template:begin imports
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+// End of section. //template:end imports
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testAccDataSource
+func TestAccDataSourceSdwanPolicyObjectDataPrefixProfileParcel(t *testing.T) {
+	if os.Getenv("SDWAN_2012") == "" {
+		t.Skip("skipping test, set environment variable SDWAN_2012")
+	}
+	var checks []resource.TestCheckFunc
+	checks = append(checks, resource.TestCheckResourceAttr("data.sdwan_policy_object_data_prefix_profile_parcel.test", "entries.0.ipv4_address", "10.0.0.0"))
+	checks = append(checks, resource.TestCheckResourceAttr("data.sdwan_policy_object_data_prefix_profile_parcel.test", "entries.0.ipv4_prefix_length", "8"))
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceSdwanPolicyObjectDataPrefixProfileParcelConfig(),
+				Check:  resource.ComposeTestCheckFunc(checks...),
+			},
+		},
+	})
+}
+
+// End of section. //template:end testAccDataSource
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testPrerequisites
+// End of section. //template:end testPrerequisites
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testAccDataSourceConfig
+func testAccDataSourceSdwanPolicyObjectDataPrefixProfileParcelConfig() string {
+	config := `resource "sdwan_policy_object_data_prefix_profile_parcel" "test" {` + "\n"
+	config += ` name = "TF_TEST"` + "\n"
+	config += ` description = "Terraform integration test"` + "\n"
+	config += `	feature_profile_id = "e4d9392a-7765-4a64-b719-a4bcaf534f25"` + "\n"
+	config += `	entries = [{` + "\n"
+	config += `	  ipv4_address = "10.0.0.0"` + "\n"
+	config += `	  ipv4_prefix_length = 8` + "\n"
+	config += `	}]` + "\n"
+	config += `}` + "\n"
+
+	config += `
+		data "sdwan_policy_object_data_prefix_profile_parcel" "test" {
+			id = sdwan_policy_object_data_prefix_profile_parcel.test.id
+			feature_profile_id = "e4d9392a-7765-4a64-b719-a4bcaf534f25"
+		}
+	`
+	return config
+}
+
+// End of section. //template:end testAccDataSourceConfig
