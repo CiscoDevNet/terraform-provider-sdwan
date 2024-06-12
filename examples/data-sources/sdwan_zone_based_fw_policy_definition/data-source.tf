2da15e4fcaed0449dcb459e113f2fea1e2f3a0a0
Sean Conroy <141843633+seconroy@users.noreply.github.com>
2024-06-12 17:47:33 +0100

    Add zone based fw policy definition resource and data source (#248)

---
+++ examples/data-sources/sdwan_zone_based_fw_policy_definition/data-source.tf
@@ -0,0 +1,3 @@
+data "sdwan_zone_based_fw_policy_definition" "example" {
+  id = "f6b2c44c-693c-4763-b010-895aa3d236bd"
+}
---
+++ examples/resources/sdwan_zone_based_fw_policy_definition/import.sh
@@ -0,0 +1,1 @@
+terraform import sdwan_zone_based_fw_policy_definition.example "f6b2c44c-693c-4763-b010-895aa3d236bd"
