d70d9b14bf115e4e6fb0c0e53231ac38ee44cb00
Sean Conroy <seconroy@cisco.com>
2024-09-04 18:29:50 +0100

    Add application priority traffic policy profile parcel resource and data source

---
+++ examples/resources/sdwan_application_priority_traffic_policy_profile_parcel/resource.tf
@@ -0,0 +1,29 @@
+resource "sdwan_application_priority_traffic_policy_profile_parcel" "example" {
+  name               = "Example"
+  description        = "My Example"
+  feature_profile_id = "f6dd22c8-0b4f-496c-9a0b-6813d1f8b8ac"
+  default_action     = "accept"
+  vpn                = ["edge_basic_vpn1"]
+  direction          = "all"
+  sequences = [
+    {
+      sequence_id   = 1
+      sequence_name = "traffic"
+      base_action   = "accept"
+      protocol      = "ipv4"
+      matches = [
+        {
+          dscp = 1
+        }
+      ]
+      actions = [
+        {
+          sets = [
+            {
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
---
+++ internal/provider/resource_sdwan_application_priority_traffic_policy_profile_parcel_test.go
@@ -0,0 +1,113 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://mozilla.org/MPL/2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+// Section below is generated&owned by "gen/generator.go". //template:begin imports
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+// End of section. //template:end imports
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testAcc
+func TestAccSdwanApplicationPriorityTrafficPolicyProfileParcel(t *testing.T) {
+	if os.Getenv("SDWAN_2012") == "" && os.Getenv("TF_VAR_policy_object_feature_template_id") == "" {
+		t.Skip("skipping test, set environment variable SDWAN_2012 or TF_VAR_policy_object_feature_template_id")
+	}
+	var checks []resource.TestCheckFunc
+	checks = append(checks, resource.TestCheckResourceAttr("sdwan_application_priority_traffic_policy_profile_parcel.test", "default_action", "accept"))
+	checks = append(checks, resource.TestCheckResourceAttr("sdwan_application_priority_traffic_policy_profile_parcel.test", "direction", "all"))
+	checks = append(checks, resource.TestCheckResourceAttr("sdwan_application_priority_traffic_policy_profile_parcel.test", "sequences.0.sequence_id", "1"))
+	checks = append(checks, resource.TestCheckResourceAttr("sdwan_application_priority_traffic_policy_profile_parcel.test", "sequences.0.sequence_name", "traffic"))
+	checks = append(checks, resource.TestCheckResourceAttr("sdwan_application_priority_traffic_policy_profile_parcel.test", "sequences.0.base_action", "accept"))
+	checks = append(checks, resource.TestCheckResourceAttr("sdwan_application_priority_traffic_policy_profile_parcel.test", "sequences.0.protocol", "ipv4"))
+	checks = append(checks, resource.TestCheckResourceAttr("sdwan_application_priority_traffic_policy_profile_parcel.test", "sequences.0.matches.0.dscp", "1"))
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+
+			{
+				Config: testAccSdwanApplicationPriorityTrafficPolicyPrerequisitesProfileParcelConfig + testAccSdwanApplicationPriorityTrafficPolicyProfileParcelConfig_all(),
+				Check:  resource.ComposeTestCheckFunc(checks...),
+			},
+		},
+	})
+}
+
+// End of section. //template:end testAcc
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testPrerequisites
+const testAccSdwanApplicationPriorityTrafficPolicyPrerequisitesProfileParcelConfig = `
+variable "policy_object_feature_template_id" {} 
+
+resource "sdwan_application_priority_feature_profile" "test" {
+  name        = "TF_TEST"
+  description = "Terraform test"
+}
+
+resource "sdwan_policy_object_policer_profile_parcel" "test" {
+  name               = "TF_TEST_POLICER"
+  description        = "My Example"
+  feature_profile_id = var.policy_object_feature_template_id
+  entries = [
+    {
+      burst_bytes   = 56500
+      exceed_action = "remark"
+      rate_bps      = 60000
+    }
+  ]
+}
+`
+
+// End of section. //template:end testPrerequisites
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testAccConfigMinimum
+
+// End of section. //template:end testAccConfigMinimum
+
+// Section below is generated&owned by "gen/generator.go". //template:begin testAccConfigAll
+func testAccSdwanApplicationPriorityTrafficPolicyProfileParcelConfig_all() string {
+	config := `resource "sdwan_application_priority_traffic_policy_profile_parcel" "test" {` + "\n"
+	config += ` name = "TF_TEST_ALL"` + "\n"
+	config += ` description = "Terraform integration test"` + "\n"
+	config += `	feature_profile_id = sdwan_application_priority_feature_profile.test.id` + "\n"
+	config += `	default_action = "accept"` + "\n"
+	config += `	vpn = ["edge_basic_vpn1"]` + "\n"
+	config += `	direction = "all"` + "\n"
+	config += `	sequences = [{` + "\n"
+	config += `	  sequence_id = 1` + "\n"
+	config += `	  sequence_name = "traffic"` + "\n"
+	config += `	  base_action = "accept"` + "\n"
+	config += `	  protocol = "ipv4"` + "\n"
+	config += `	  matches = [{` + "\n"
+	config += `		dscp = 1` + "\n"
+	config += `	}]` + "\n"
+	config += `	  actions = [{` + "\n"
+	config += `      sets = [{` + "\n"
+	config += `		}]` + "\n"
+	config += `	}]` + "\n"
+	config += `	}]` + "\n"
+	config += `}` + "\n"
+	return config
+}
+
+// End of section. //template:end testAccConfigAll
