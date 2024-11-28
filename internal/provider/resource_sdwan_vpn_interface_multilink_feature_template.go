02757ed89d1b9c1ff98d4a884c1fa3a9a10842d2
Sean Conroy <seconroy@cisco.com>
2024-11-28 17:29:42 +0000

    Add missed feature template

--- internal/provider/resource_sdwan_vpn_interface_multilink_feature_template.go
+++ internal/provider/resource_sdwan_vpn_interface_multilink_feature_template.go
@@ -331,10 +331,10 @@ func (r *VPNInterfaceMultilinkFeatureTemplateResource) Schema(ctx context.Contex
 				Optional:            true,
 			},
 			"tunnel_qos_mode": schema.StringAttribute{
-				MarkdownDescription: helpers.NewAttributeDescription("Set tunnel QoS mode").AddStringEnumDescription("spoke").String,
+				MarkdownDescription: helpers.NewAttributeDescription("Set tunnel QoS mode").AddStringEnumDescription("hub", "spoke").String,
 				Optional:            true,
 				Validators: []validator.String{
-					stringvalidator.OneOf("spoke"),
+					stringvalidator.OneOf("hub", "spoke"),
 				},
 			},
 			"tunnel_qos_mode_variable": schema.StringAttribute{
