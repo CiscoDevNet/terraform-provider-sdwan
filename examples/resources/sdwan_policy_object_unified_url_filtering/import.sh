027706e0c26279e295b043a6ffe26b7576deaade
Sean Conroy <141843633+seconroy@users.noreply.github.com>
2025-10-21 11:43:53 +0100

    Add missing unified security policy objects (#544)

---
+++ examples/resources/sdwan_policy_object_unified_url_filtering/import.sh
@@ -0,0 +1,2 @@
+# Expected import identifier with the format: "policy_object_unified_url_filtering_id,feature_profile_id"
+terraform import sdwan_policy_object_unified_url_filtering.example "f6b2c44c-693c-4763-b010-895aa3d236bd,f6dd22c8-0b4f-496c-9a0b-6813d1f8b8ac"
