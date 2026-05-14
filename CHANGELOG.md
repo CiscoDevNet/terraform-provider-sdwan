f0fb1f75a4509651fd51135b50600f93c4b4ce9d
Wei Li <geniusangel@163.com>
2026-05-14 15:26:23 +0800

    Validate that the URL is in proper format without a "/" at the end (#696)
    * Add validation to ensure that the SDWAN_URL is in proper format without a `/` at the end
    
    * improve a bit the error message
    
    ---------
    
    Co-authored-by: wei li <wli6@cisco.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
 - Bump `sdwan_system_aaa_feature` schema to `20.18.0`
 - Fix issue where `sdwan_cflowd_policy_definition` allows  `all` instead of `both` for `protocol` field
 - Add `sdwan_cloud_provider_settings` resource and data source
+- Add validation to ensure that the SDWAN_URL is in proper format without a `/` at the end
 
 ## 0.11.1
 
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -23,6 +23,7 @@ import (
 	"log/slog"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
@@ -198,6 +199,15 @@ func (p *SdwanProvider) Configure(ctx context.Context, req provider.ConfigureReq
 		resp.Diagnostics.AddError(
 			"Unable to find url",
 			"URL cannot be an empty string",
+		)
+		return
+	}
+
+	// Validate that the URL is in proper format without a "/" at the end
+	if strings.HasSuffix(url, "/") {
+		resp.Diagnostics.AddError(
+			"Invalid URL format",
+			"URL '"+url+"' cannot end with a trailing slash ('/')",
 		)
 		return
 	}
