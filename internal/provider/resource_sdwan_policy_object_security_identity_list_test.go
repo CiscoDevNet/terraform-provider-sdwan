1357a7dc50c0ab5fe6a2e6971b6088a90fa95ca3
Anatolii Kovalchuk <47783386+koval4ukav@users.noreply.github.com>
2026-07-14 10:33:44 +0200

    ISE-tests-update

--- internal/provider/resource_sdwan_policy_object_security_identity_list_test.go
+++ internal/provider/resource_sdwan_policy_object_security_identity_list_test.go
@@ -29,8 +29,8 @@ import (
 
 // Section below is generated&owned by "gen/generator.go". //template:begin testAcc
 func TestAccSdwanPolicyObjectSecurityIdentityListProfileParcel(t *testing.T) {
-	if os.Getenv("SDWAN_2015") == "" {
-		t.Skip("skipping test, set environment variable SDWAN_2015")
+	if os.Getenv("SDWAN_ISE") == "" {
+		t.Skip("skipping test, set environment variable SDWAN_ISE")
 	}
 	var checks []resource.TestCheckFunc
 	checks = append(checks, resource.TestCheckResourceAttr("sdwan_policy_object_security_identity_list.test", "entries.0.user", "administrator"))
